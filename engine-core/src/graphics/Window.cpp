08a999e1a6337a946bce6f9a3169ee09cc4498b3
gavingregory <gavin.ian.gregory@gmail.com>
2016-02-14 15:21:33 +0000

    Check for OpenGL errors every rendered frame.

--- engine-core/src/graphics/Window.cpp
+++ engine-core/src/graphics/Window.cpp
@@ -20,6 +20,10 @@ namespace engine {
 
 		void Window::update()
 		{
+			GLenum error = glGetError();
+			if (error != GL_NO_ERROR)
+				std::cout << "OpenGL Error: " << error << std::endl;
+
 			glfwPollEvents();
 			glfwSwapBuffers(m_Window);
 		}
