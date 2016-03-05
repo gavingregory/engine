d429e2b4490fe56e16d615cdff5ee95309268546
Gavin Gregory <gavin.ian.gregory@gmail.com>
2016-03-05 19:13:38 +0000

    Fixed textures. Textures now load correctly, and whichever textures are defined as used by the entity should get loaded to the shader.

--- engine-common/src/graphics/Texture.h
+++ engine-common/src/graphics/Texture.h
@@ -8,8 +8,6 @@ class Texture {
 public:
 	static GLuint Load(string path) {
 		GLuint t = SOIL_load_OGL_texture(path.c_str(), 0, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT);
-		glGenTextures(1, &t);
-		if (t == 0) { cout << "Error loading texture " << path << " with SOIL."; }
 		return t;
 	}
 };
